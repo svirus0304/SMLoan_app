cf6d9ddcd1e000d7e5810095af6681940781caf7
svirus0304 <svirus0304@naver.com>
2016-11-22 09:31:50 +0900

    Initial commit

---
+++ app/src/main/java/com/sumer/loan/smloan/act_main.java
@@ -0,0 +1,13 @@
+package com.sumer.loan.smloan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class act_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lay_main);
+    }
+}

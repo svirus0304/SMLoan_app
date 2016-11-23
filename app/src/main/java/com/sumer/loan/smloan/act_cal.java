06ffda96aee4be7d615ad4330f87074ab7c80bdf
svirus0304 <svirus0304@naver.com>
2016-11-23 15:41:44 +0900

    main layout done

--- app/src/main/java/com/sumer/loan/smloan/act_cal.java
+++ app/src/main/java/com/sumer/loan/smloan/act_cal.java
@@ -1,7 +1,9 @@
 package com.sumer.loan.smloan;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * Created by kjb on 2016-11-23.
@@ -12,5 +14,17 @@ public class act_cal extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lay_cal);
+
+        //cal_cnt_txtv, cal_mem_txtv
+
+        TextView cal_cnt_txtv=(TextView)findViewById(R.id.cal_cnt_txtv);
+        TextView cal_mem_txtv=(TextView)findViewById(R.id.cal_mem_txtv);
+
+        Intent intent=new Intent();
+        String main_cnt=intent.getStringExtra("main_cnt");
+        String main_mem=intent.getStringExtra("main_mem");
+
+        cal_cnt_txtv.setText(main_cnt);
+        cal_mem_txtv.setText(main_mem);
     }
 }

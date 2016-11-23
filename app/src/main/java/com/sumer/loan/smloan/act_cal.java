26b94ab398dd327882922d051487a5bd58d104a2
svirus0304 <svirus0304@naver.com>
2016-11-23 15:35:29 +0900

    main layout done

---
+++ app/src/main/java/com/sumer/loan/smloan/act_cal.java
@@ -0,0 +1,16 @@
+package com.sumer.loan.smloan;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by kjb on 2016-11-23.
+ */
+
+public class act_cal extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lay_cal);
+    }
+}
--- app/src/main/java/com/sumer/loan/smloan/act_main.java
+++ app/src/main/java/com/sumer/loan/smloan/act_main.java
@@ -1,7 +1,14 @@
 package com.sumer.loan.smloan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+
+import org.w3c.dom.Text;
 
 public class act_main extends AppCompatActivity {
 
@@ -9,5 +16,23 @@ public class act_main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lay_main);
+
+        Button main_btn_ok=(Button)findViewById(R.id.main_btn_ok);
+        main_btn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NumberPicker main_cnt_numpicker=(NumberPicker)findViewById(R.id.main_cnt_numpicker);
+                int main_cnt=(int)main_cnt_numpicker.getValue();
+
+                EditText main_mem_txt=(EditText)findViewById(R.id.main_mem_txt);
+                String main_mem=main_mem_txt.getText().toString();
+
+                Intent intent=new Intent(act_main.this,act_cal.class);
+                intent.putExtra("main_cnt",main_cnt);
+                intent.putExtra("main_mem",main_mem);
+                startActivity(intent);
+
+            }
+        });
     }
 }
